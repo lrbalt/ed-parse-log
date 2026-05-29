121e3081e725a8af30d248b809cd794ecef6f906
Reinier Balt <lrbalt@gmail.com>
2026-05-29 13:41:33 +0200

    parse Status event that can be found in status.json

--- src/log_line.rs
+++ src/log_line.rs
@@ -156,6 +156,148 @@ pub struct EDLogMusic {
     music_track: EDString,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "PascalCase", deny_unknown_fields)]
+pub struct FuelStatus {
+    fuel_main: f64,
+    fuel_reservoir: f64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "PascalCase", deny_unknown_fields)]
+pub struct Destination {
+    system: u64,
+    body: u64,
+    name: EDString,
+    #[serde(rename = "Name_Localised")]
+    name_localised: Option<EDString>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum LegalState {
+    Clean,
+    IllegalCargo,
+    Speeding,
+    Wanted,
+    Hostile,
+    PassengerWanted,
+}
+
+/*
+
+TODO:
+Flags:
+Bit Value      Hex       Meaning
+0   1          0000 0001 Docked, (on a landing pad)
+1   2          0000 0002 Landed, (on planet surface)
+2   4          0000 0004 Landing Gear Down
+3   8          0000 0008 Shields Up
+4   16         0000 0010 Supercruise
+5   32         0000 0020 FlightAssist Off
+6   64         0000 0040 Hardpoints Deployed
+7   128        0000 0080 In Wing
+8   256        0000 0100 LightsOn
+9   512        0000 0200 Cargo Scoop Deployed
+10  1024       0000 0400 Silent Running,
+11  2048       0000 0800 Scooping Fuel
+12  4096       0000 1000 Srv Handbrake
+13  8192       0000 2000 Srv using Turret view
+14  16384      0000 4000 Srv Turret retracted (close to ship)
+15  32768      0000 8000 Srv DriveAssist
+16  65536      0001 0000 Fsd MassLocked
+17  131072     0002 0000 Fsd Charging
+18  262144     0004 0000 Fsd Cooldown
+19  524288     0008 0000 Low Fuel ( < 25% )
+20  1048576    0010 0000 Over Heating ( > 100% )
+21  2097152    0020 0000 Has Lat Long
+22  4194304    0040 0000 IsInDanger
+23  8388608    0080 0000 Being Interdicted
+24  16777216   0100 0000 In MainShip
+25  33554432   0200 0000 In Fighter
+26  67108864   0400 0000 In SRV
+27  134217728  0800 0000 Hud in Analysis mode
+28  268435456  1000 0000 Night Vision
+29  536870912  2000 0000 Altitude from Average radius
+30  1073741824 4000 0000 fsdJump
+31  2147483648 8000 0000 srvHighBeam
+
+Flags2:
+Bit Value      Hex       Meaning
+0   1          0000 0001 OnFoot
+1   2          0000 0002 InTaxi (or dropship/shuttle)
+2   4          0000 0004 InMulticrew (ie in someone else’s ship)
+3   8          0000 0008 OnFootInStation
+4   16         0000 0010 OnFootOnPlanet
+5   32         0000 0020 AimDownSight
+6   64         0000 0040 LowOxygen
+7   128        0000 0080 LowHealth
+8   256        0000 0100 Cold
+9   512        0000 0200 Hot
+10  1024       0000 0400 VeryCold
+11  2048       0000 0800 VeryHot
+12  4096       0000 1000 Glide Mode
+13  8192       0000 2000 OnFootInHangar
+14  16384      0000 4000 OnFootSocialSpace
+15  32768      0000 8000 OnFootExterior
+16  65536      0001 0001 BreathableAtmosphere
+17  131072     0002 0000 Telepresence Multicrew
+18  262144     0004 0000 Physical Multicrew
+19  524288     0008 0000 Fsd hyperdrive charging
+20  1048576    0010 0000 Supercruise Overcharge
+21  2097152    0020 0000 Supercruise Assist
+22  4194304    0040 0000 NPC Crew Active
+
+Guifocus
+
+0  NoFocus
+1  InternalPanel (right hand side)
+2  ExternalPanel (left hand side)
+3  CommsPanel (top)
+4  RolePanel (bottom)
+5  StationServices
+6  GalaxyMap
+7  SystemMap
+8  Orrery
+9  FSS mode
+10 SAA mode
+11 Codex
+
+*/
+
+#[derive(Serialize, Deserialize, Clone, Debug, Extractable)]
+#[serde(rename_all = "PascalCase", deny_unknown_fields)]
+#[testcase({ "timestamp":"2017-12-07T10:31:37Z", "event":"Status", "Flags":16842765, "Pips":[2,8,2], "FireGroup":0, 
+    "Fuel":{ "FuelMain":15.146626, "FuelReservoir":0.382796 }, "GuiFocus":5 })]
+#[testcase({ "timestamp":"2017-12-07T12:03:14Z", "event":"Status", "Flags":18874376, "Pips":[4,8,0], "FireGroup":0,
+    "Fuel":{ "FuelMain":15.146626, "FuelReservoir":0.382796 }, "GuiFocus":0, "Latitude":-28.584963,
+    "Longitude":6.826313, "Heading":109, "Altitude": 404 })]
+#[testcase({ "timestamp":"2026-05-15T12:51:43Z", "event":"Status", "Flags":0 })]
+pub struct EDLogStatus {
+    flags: u64,
+    flags2: Option<u64>,
+    pips: Option<[u8; 3]>,
+    fire_group: Option<u64>,
+    fuel: Option<FuelStatus>,
+    gui_focus: Option<u64>,
+    latitude: Option<f64>,
+    longitude: Option<f64>,
+    heading: Option<u64>,
+    altitude: Option<f64>,
+    cargo: Option<u64>,
+    legal_state: Option<LegalState>,
+    balance: Option<Credits>,
+    oxigen: Option<f64>,
+    health: Option<f64>,
+    temperature: Option<f64>,
+    selected_weapon: Option<EDString>,
+    #[serde(rename = "SelectedWeapon_Localised")]
+    selected_weapon_localised: Option<EDString>,
+    body_name: Option<EDString>,
+    destination: Option<Destination>,
+    planet_radius: Option<f64>,
+    gravity: Option<f64>,
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug, Extractable)]
 #[serde(rename_all = "PascalCase", deny_unknown_fields)]
 #[testcase({ "timestamp":"2023-07-30T14:40:36Z", "event":"ReceiveText", "From":"$ShipName_PassengerLiner_Cruise;", 
@@ -303,6 +445,7 @@ pub enum EDLogEvent {
     SystemsShutdown,
     Shutdown,
     Music(EDLogMusic),
+    Status(EDLogStatus),
 
     // Commander
     Commander(EDLogCommander),
