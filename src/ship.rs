db4abc8b8f2e31c93d61d9d08d5441f3a67c74b9
Reinier Balt <lrbalt@gmail.com>
2026-06-05 16:19:44 +0200

    make Module an enum for Afmu event

--- src/ship.rs
+++ src/ship.rs
@@ -1,6 +1,7 @@
 use crate::{EDString, common_types::PilotRank, market::MarketItemType};
 use ed_parse_log_files_macros::{Extractable, testcase, testcase_struct};
 use serde::{Deserialize, Serialize};
+use strum::Display;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename_all = "PascalCase", deny_unknown_fields)]
@@ -277,12 +278,243 @@ pub struct EDLogSynthesis {
     materials: Vec<SynthesisMaterial>,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, Display)]
+#[serde(rename_all = "PascalCase", deny_unknown_fields)]
+pub enum ShipModule {
+    #[strum(to_string = "FSD (SCO)")]
+    #[serde(alias = "$int_hyperdrive_overcharge_size5_class5_name;")]
+    FSDSCOS5C5,
+    #[strum(to_string = "FSD (SCO)")]
+    #[serde(alias = "$int_hyperdrive_overcharge_size8_class5_overchargebooster_mkii_name;")]
+    FSDSCOS8C5OverchargeBoosterMkII,
+    #[strum(to_string = "FSD (SCO)")]
+    #[serde(alias = "$int_hyperdrive_overcharge_size6_class5_name;")]
+    FSDSCOS6C5,
+    #[strum(to_string = "Surface Scanner")]
+    #[serde(alias = "$int_detailedsurfacescanner_tiny_name;")]
+    SurfaceScanner,
+    #[strum(to_string = "Guardian FSD Booster")]
+    #[serde(alias = "$int_guardianfsdbooster_size5_name;")]
+    GuardianFSDBooster,
+    #[strum(to_string = "Thrusters")]
+    #[serde(alias = "$int_engine_size6_class5_name;")]
+    ThrustersS6C5,
+    #[strum(to_string = "Thrusters")]
+    #[serde(alias = "$int_engine_size5_class5_name;")]
+    ThrustersS5C5,
+    #[strum(to_string = "Thrusters")]
+    #[serde(alias = "$int_engine_size7_class5_gravityoptimised_mkii_name;")]
+    ThrustersS7C5GravityOptimisedMkII,
+    #[strum(to_string = "Fighter Hangar")]
+    #[serde(alias = "$int_fighterbay_size5_class1_name;")]
+    FighterHangar,
+    #[strum(to_string = "Heatsink")]
+    #[serde(alias = "$hpt_heatsinklauncher_turret_tiny_name;")]
+    Heatsink,
+    #[strum(to_string = "Supercruise Assist")]
+    #[serde(alias = "$int_supercruiseassist_name;")]
+    SupercruiseAssist,
+    #[strum(to_string = "Repair")]
+    #[serde(alias = "$int_dronecontrol_repair_size3_class2_name;")]
+    RepairS3C2,
+    #[strum(to_string = "Repair")]
+    #[serde(alias = "$int_dronecontrol_repair_size5_class2_name;")]
+    RepairS5C2,
+    #[strum(to_string = "Recon")]
+    #[serde(alias = "$int_dronecontrol_recon_size1_class1_name;")]
+    ReconS1C1,
+    #[strum(to_string = "Research")]
+    #[serde(alias = "$int_dronecontrol_unkvesselresearch_name;")]
+    Research,
+    #[strum(to_string = "Docking Computer")]
+    #[serde(alias = "$int_dockingcomputer_advanced_name;")]
+    DockingComputer,
+    #[strum(to_string = "Cargo Hatch")]
+    #[serde(alias = "$modularcargobaydoorfdl_name;")]
+    CargoHatchFDL,
+    #[strum(to_string = "Cargo Hatch")]
+    #[serde(alias = "$modularcargobaydoor_name;")]
+    CargoHatch,
+    #[strum(to_string = "Life Support")]
+    #[serde(alias = "$int_lifesupport_size4_class5_name;")]
+    LifeSupportS4C5,
+    #[strum(to_string = "Life Support")]
+    #[serde(alias = "$int_lifesupport_size4_class2_name;")]
+    LifeSupportS4C2,
+    #[strum(to_string = "Life Support")]
+    #[serde(alias = "$int_lifesupport_size5_class2_name;")]
+    LifeSupportS5C2,
+    #[strum(to_string = "Life Support")]
+    #[serde(alias = "$int_lifesupport_size5_class5_name;")]
+    LifeSupportS5C5,
+    #[strum(to_string = "Fuel Scoop")]
+    #[serde(alias = "$int_fuelscoop_size6_class5_name;")]
+    FuelScoopS6C5,
+    #[strum(to_string = "Fuel Scoop")]
+    #[serde(alias = "$int_fuelscoop_size7_class5_name;")]
+    FuelScoopS7C5,
+    #[strum(to_string = "Sensors")]
+    #[serde(alias = "$int_sensors_size5_class2_name;")]
+    SensorsS5C2,
+    #[strum(to_string = "Sensors")]
+    #[serde(alias = "$int_sensors_size6_class2_name;")]
+    SensorsS6C2,
+    #[strum(to_string = "Sensors")]
+    #[serde(alias = "$int_sensors_size8_class2_name;")]
+    SensorsS8C2,
+    #[strum(to_string = "Power Distributor")]
+    #[serde(alias = "$int_powerdistributor_size6_class5_name;")]
+    PowerDistributorS6C5,
+    #[strum(to_string = "Power Distributor")]
+    #[serde(alias = "$int_powerdistributor_size5_class5_name;")]
+    PowerDistributorS5C5,
+    #[strum(to_string = "Power Distributor")]
+    #[serde(alias = "$int_powerdistributor_size7_class5_name;")]
+    PowerDistributorS7C5,
+    #[strum(to_string = "Planetary Vehicle Hangar")]
+    #[serde(alias = "$int_buggybay_size4_class2_name;")]
+    PlanetaryVehicleHangar,
+    #[strum(to_string = "Remote Flak")]
+    #[serde(alias = "$hpt_flakmortar_fixed_medium_name;")]
+    RemoteFlakFixedMedium,
+    #[strum(to_string = "Remote Flak")]
+    #[serde(alias = "$hpt_flakmortar_turret_medium_name;")]
+    RemoteFlakTurretMedium,
+    #[strum(to_string = "AFM Unit")]
+    #[serde(alias = "$int_repairer_size6_class5_name;")]
+    AFMUnit,
+    #[strum(to_string = "Bi-Weave Shield")]
+    #[serde(alias = "$int_shieldgenerator_size6_class3_fast_name;")]
+    BiWeaveShieldS6C3,
+    #[strum(to_string = "Prismatic Shield")]
+    #[serde(alias = "$int_shieldgenerator_size3_class5_strong_name;")]
+    PrismaticShieldS3C5,
+    #[strum(to_string = "Prismatic Shield")]
+    #[serde(alias = "$int_shieldgenerator_size6_class5_strong_name;")]
+    PrismaticShieldS6C5,
+    #[strum(to_string = "Shield Generator")]
+    #[serde(alias = "$int_shieldgenerator_size6_class2_name;")]
+    ShieldGeneratorS6C2,
+    #[strum(to_string = "Shield Generator")]
+    #[serde(alias = "$int_shieldgenerator_size5_class2_name;")]
+    ShieldGeneratorS5C2,
+    #[strum(to_string = "Cockpit")]
+    #[serde(alias = "$typex_cockpit_name;")]
+    TypeXCockpit,
+    #[strum(to_string = "Cockpit")]
+    #[serde(alias = "$python_nx_cockpit_name;")]
+    PythonMkIICockpit,
+    #[strum(to_string = "Cockpit")]
+    #[serde(alias = "$explorer_nx_cockpit_name;")]
+    CaspianExplorerCockpit,
+    #[strum(to_string = "K-Warrant Scanner")]
+    #[serde(alias = "$hpt_crimescanner_size0_class5_name;")]
+    KWarrantScanner,
+    #[strum(to_string = "Rail Gun")]
+    #[serde(alias = "$hpt_railgun_fixed_medium_name;")]
+    RailGun,
+    #[strum(to_string = "Thrusters")]
+    #[serde(alias = "$int_engine_size7_class5_name;")]
+    ThrustersS7C5,
+    #[strum(to_string = "Guardian Module Reinforcement")]
+    #[serde(alias = "$int_guardianmodulereinforcement_size5_class2_name;")]
+    GuardianModuleReinforcementS5C2,
+    #[strum(to_string = "Module Reinforcement")]
+    #[serde(alias = "$int_modulereinforcement_size3_class2_name;")]
+    ModuleReinforcementS3C2,
+    #[strum(to_string = "Module Reinforcement")]
+    #[serde(alias = "$int_modulereinforcement_size4_class2_name;")]
+    ModuleReinforcementS4C2,
+    #[strum(to_string = "Module Reinforcement")]
+    #[serde(alias = "$int_modulereinforcement_size1_class2_name;")]
+    ModuleReinforcementS1C2,
+    #[strum(to_string = "Module Reinforcement")]
+    #[serde(alias = "$int_modulereinforcement_size2_class2_name;")]
+    ModuleReinforcementS2C2,
+    #[strum(to_string = "Module Reinforcement")]
+    #[serde(alias = "$int_modulereinforcement_size5_class2_name;")]
+    ModuleReinforcementS5C2,
+    #[strum(to_string = "Shield Booster")]
+    #[serde(alias = "$hpt_shieldbooster_size0_class1_name;")]
+    ShieldBoosterS0C1,
+    #[strum(to_string = "Shield Booster")]
+    #[serde(alias = "$hpt_shieldbooster_size0_class5_name;")]
+    ShieldBoosterS0C5,
+    #[strum(to_string = "Power Distributor")]
+    #[serde(alias = "$int_powerdistributor_size8_class5_name;")]
+    PowerDistributorS8C5,
+    #[strum(to_string = "Beam Laser")]
+    #[serde(alias = "$hpt_beamlaser_gimbal_medium_name;")]
+    BeamLaserMedium,
+    #[strum(to_string = "Beam Laser")]
+    #[serde(alias = "$hpt_beamlaser_gimbal_small_name;")]
+    BeamLaserSmall,
+    #[strum(to_string = "Gauss Cannon")]
+    #[serde(alias = "$hpt_guardian_gausscannon_fixed_medium_name;")]
+    GaussCannonFixedMedium,
+    #[strum(to_string = "Gauss Cannon")]
+    #[serde(alias = "$hpt_guardian_gausscannon_fixed_small_name;")]
+    GaussCannonFixedSmall,
+    #[strum(to_string = "Shard Cannon")]
+    #[serde(alias = "$hpt_guardian_shardcannon_fixed_large_name;")]
+    ShardCannonFixedLarge,
+    #[strum(to_string = "Shard Cannon")]
+    #[serde(alias = "$hpt_guardian_shardcannon_fixed_medium_name;")]
+    ShardCannonFixedMedium,
+    #[strum(to_string = "Plasma Charger")]
+    #[serde(alias = "$hpt_guardian_plasmalauncher_fixed_medium_name;")]
+    PlasmaChargerFixedMedium,
+    #[strum(to_string = "Enhanced Xeno Scanner")]
+    #[serde(alias = "$hpt_xenoscannermk2_basic_tiny_name;")]
+    EnhancedXenoScanner,
+    #[strum(to_string = "Caustic Sink Launcher")]
+    #[serde(alias = "$hpt_causticsinklauncher_turret_tiny_name;")]
+    CausticSinkLauncher,
+    #[strum(to_string = "Enhanced AX Multi-Cannon")]
+    #[serde(alias = "$hpt_atmulticannon_gimbal_medium_name;")]
+    EnhancedAXMultiCannonGimbalMedium,
+    #[strum(to_string = "Enhanced AX Multi-Cannon")]
+    #[serde(alias = "$hpt_atmulticannon_gimbal_large_name;")]
+    EnhancedAXMultiCannonGimbalLarge,
+    #[strum(to_string = "TG Pulse Neutraliser")]
+    #[serde(alias = "$hpt_antiunknownshutdown_tiny_v2_name;")]
+    TGPulseNeutraliser,
+    #[strum(to_string = "Shutdown Field Neutraliser")]
+    #[serde(alias = "$hpt_antiunknownshutdown_tiny_name;")]
+    ShutdownFieldNeutraliser,
+    #[strum(to_string = "Seeker Missile Rack")]
+    #[serde(alias = "$hpt_basicmissilerack_fixed_small_name;")]
+    SeekerMissileRackFixedSmall,
+    #[strum(to_string = "Pack-Hound")]
+    #[serde(alias = "$hpt_drunkmissilerack_fixed_medium_name;")]
+    PackHoundFixedMedium,
+    #[strum(to_string = "Missile Rack")]
+    #[serde(alias = "$hpt_dumbfiremissilerack_fixed_medium_name;")]
+    MissileRackFixedMedium,
+    #[strum(to_string = "AX Missile Rack")]
+    #[serde(alias = "$hpt_atdumbfiremissile_fixed_medium_name;")]
+    AXMissileRackFixeMedium,
+    #[strum(to_string = "Enhanced AX Missile Rack")]
+    #[serde(alias = "$hpt_atdumbfiremissile_fixed_medium_v2_name;")]
+    EnhancedAXMissileRackFixedMediumV2,
+    #[strum(to_string = "Enhanced AX Missile Rack")]
+    #[serde(alias = "$hpt_atdumbfiremissile_fixed_large_v2_name;")]
+    EnhancedAXMissileRackFixedLargeV2,
+    #[strum(to_string = "Universal Multi-Limpet Controller")]
+    #[serde(alias = "$int_multidronecontrol_universal_size7_class5_name;")]
+    UniversalMultiLimpetController,
+    #[strum(to_string = "Nanite Torpedo Pylon")]
+    #[serde(alias = "$hpt_atventdisruptorpylon_fixed_medium_name;")]
+    NaniteTorpedoPylon,
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug, Extractable)]
 #[serde(rename_all = "PascalCase", deny_unknown_fields)]
 #[testcase({ "timestamp":"2026-01-23T15:42:09Z", "event":"AfmuRepairs", "Module":"$explorer_nx_cockpit_name;", 
     "FullyRepaired":true, "Health":1.000000 })]
+#[testcase({ "timestamp":"2026-03-27T18:42:08Z", "event":"AfmuRepairs", "Module":"$int_dockingcomputer_advanced_name;", "Module_Localised":"Docking Computer", "FullyRepaired":true, "Health":1.000000 })]
 pub struct EDLogAfmuRepairs {
-    pub module: EDString,
+    pub module: ShipModule,
     #[serde(rename = "Module_Localised")]
     pub module_localised: Option<EDString>,
     pub fully_repaired: bool,
